ca010b7f4f59ac144cae70e94c5a2cde87437014
peter346 <peter346@wwu.edu>
2020-02-10 22:28:33 -0800

    Changed from Running ExampleBot

--- StarCraftBot/src/main/java/StrategyAgent.java
+++ StarCraftBot/src/main/java/StrategyAgent.java
@@ -132,6 +132,6 @@ public class StrategyAgent extends DefaultBWListener{
     }
 
     public static void main(String[] args) {
-        new ExampleBot().run();
+        new StrategyAgent().run();
     }
 }
