d9b4672580fbfdaa43e83d201e2a137efd4f672c
Chris Lokken <lokkenc@wwu.edu>
2020-03-03 14:49:16 -0800

    Added more to the planning reward function and reformatted to make code a bit cleaner

--- StarCraftBot/src/main/java/Planning/PlanningRewardFunction.java
+++ StarCraftBot/src/main/java/Planning/PlanningRewardFunction.java
@@ -31,81 +31,80 @@ public class PlanningRewardFunction implements RewardFunction {
         int targetArmySize = 50;
         int maxTimeSinceLastScout = 3600;
         double reward = 0.0;
-        //Action is an attack
-        if (a.actionName().equals("AttackAction")) {
-
-        }
-        //Action is building a structure
-        else if (a.actionName().equals("BuildAction")) {
-            
-        }
-        //Action is expanding (building a new Hatchery/Nexus)
-        else if (a.actionName().equals("ExpandAction")) {
-            if ((int) s.get("numBases") < 2) {
-                //Give reward if preconditions for expanding are met
-                if ((int) s.get("numWorkers") >= targetNumWorkers && (int) s.get("mineralProductionRate") >= targetMineralProduction
-                        && (int) s.get("gasProductionRate") >= targetGasProduction) {
-                    reward += 500;
-                }
-                else {
+        switch(a.actionName()) {
+            case "AttackAction" :
+                if ((Boolean) sprime.get("attackingEnemyBase") == true) {
                     reward -= 500;
+                } else { //We are attacking intruders
+                    reward += 100;
                 }
-            } else {
-                reward -= 500;
-            }
-        }
-        //Action is scouting
-        else if (a.actionName().equals("ScoutAction")) {
-            //Checks the last time scouted and gives reward based on that
-            if ((int) s.get("timeSinceLastScout") > maxTimeSinceLastScout) {
-                reward += (int) s.get("timeSinceLastScout") / 100;
-            } else {
-                reward -= (int) s.get("timeSinceLastScout") / 100;
-            }
+                break;
+            case "BuildAction":
+                //Get information of what is being built?
 
-        }
-        //Action is training a new unit
-        else if (a.actionName().equals("TrainAction")) {
-            int[][] trainingCapacity = (int[][]) s.get("trainingCapacity");
-            //Checking worker status
-            if ((int) sprime.get("numWorkers") > (int) s.get("numWorkers")) {
-                //Disincentivize queueing units that cannot be trained
-                if (trainingCapacity[0][1] == 0)
-                    reward -= 100;
-                //Given or taking reward based on if the action is training worker
-                 if ((int) s.get("numWorkers") < targetNumWorkers)
-                    reward += targetNumWorkers - (int) s.get("numWorkers");
-                else
-                    reward -= 100;
-            }
-
-            //Getting size of current army
-            ArrayList<CombatUnitStatus> combatList = (ArrayList<CombatUnitStatus>) s.get("combatUnitStatuses");
-            int curArmy = 0;
-            for (CombatUnitStatus combatUnitStatus: combatList) {
-                curArmy += combatUnitStatus.getAmount();
-            }
-            //Getting size of army after action
-            combatList = (ArrayList<CombatUnitStatus>) sprime.get("combatUnitStatuses");
-            int sprimeArmy = 0;
-            for (CombatUnitStatus combatUnitStatus: combatList) {
-                sprimeArmy += combatUnitStatus.getAmount();
-            }
-
-            //Evaluating reward based on army
-            if (sprimeArmy > curArmy) {
-                if (curArmy < targetArmySize) {
-                    reward += targetArmySize - curArmy;
+                //PLACEHOLDER REWARD
+                reward += 50;
+                break;
+            case "ExpandAction":
+                if ((int) s.get("numBases") < 2) {
+                    //Give reward if preconditions for expanding are met
+                    if ((int) s.get("numWorkers") >= targetNumWorkers && (int) s.get("mineralProductionRate") >= targetMineralProduction
+                            && (int) s.get("gasProductionRate") >= targetGasProduction) {
+                        reward += 500;
+                    }
+                    else {
+                        reward -= 500;
+                    }
                 } else {
-                    reward -= 100;
+                    reward -= 500;
+                } break;
+            case "ScoutAction" :
+                //Checks the last time scouted and gives reward based on that
+                if ((int) s.get("timeSinceLastScout") > maxTimeSinceLastScout) {
+                    reward += (int) s.get("timeSinceLastScout") / 100;
+                } else {
+                    reward -= (int) s.get("timeSinceLastScout") / 100;
                 }
-            }
+                break;
+            case "TrainAction":
+                int[][] trainingCapacity = (int[][]) s.get("trainingCapacity");
+                //Checking worker status
+                if ((int) sprime.get("numWorkers") > (int) s.get("numWorkers")) {
+                    //Disincentivize queueing units that cannot be trained
+                    if (trainingCapacity[0][1] == 0)
+                        reward -= 100;
+                    //Given or taking reward based on if the action is training worker
+                    if ((int) s.get("numWorkers") < targetNumWorkers)
+                        reward += targetNumWorkers - (int) s.get("numWorkers");
+                    else
+                        reward -= 100;
+                }
 
+                //Getting size of current army
+                ArrayList<CombatUnitStatus> combatList = (ArrayList<CombatUnitStatus>) s.get("combatUnitStatuses");
+                int curArmy = 0;
+                for (CombatUnitStatus combatUnitStatus: combatList) {
+                    curArmy += combatUnitStatus.getAmount();
+                }
+                //Getting size of army after action
+                combatList = (ArrayList<CombatUnitStatus>) sprime.get("combatUnitStatuses");
+                int sprimeArmy = 0;
+                for (CombatUnitStatus combatUnitStatus: combatList) {
+                    sprimeArmy += combatUnitStatus.getAmount();
+                }
 
-        }
-        //Action is upgrading a unit
-        else if (a.actionName().equals("UpgradeAction")) {
-
+                //Evaluating reward based on army
+                if (sprimeArmy > curArmy) {
+                    if (curArmy < targetArmySize) {
+                        reward += targetArmySize - curArmy;
+                    } else {
+                        reward -= 100;
+                    }
+                }
+                break;
+            case "UpgradeAction":
+                reward -= 100;
+                break;
         }
         return reward;
     }
