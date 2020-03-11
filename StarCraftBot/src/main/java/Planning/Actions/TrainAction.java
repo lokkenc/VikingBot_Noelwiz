e1ecae9d63756ce7b98af3d088a32759f92d9ae3
peter346 <peter346@wwu.edu>
2020-03-11 13:43:34 -0700

    Enforced Rules, used state

--- StarCraftBot/src/main/java/Planning/Actions/TrainAction.java
+++ StarCraftBot/src/main/java/Planning/Actions/TrainAction.java
@@ -2,22 +2,74 @@ package Planning.Actions;
 
 import burlap.mdp.core.action.Action;
 
+import java.util.Objects;
+import java.util.Scanner;
+
 public class TrainAction implements Action {
     private String actionName;
     private static final String BaseActionName = "TrainAction";
+    private static final String[] possibleUnits = new String[]{"worker","combatUnit"};
 
-    //TODO: ALLOW for options to train worker, or a specific unit by name
-    //possibly could replace using the names with a unit catigoriazation we make
-    //that the bot then interperates and chooses
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof TrainAction)) return false;
+        TrainAction that = (TrainAction) o;
+        return Objects.equals(actionName, that.actionName);
+    }
+
 
     /**
-     *
-     * @param s stirng of options seperated by '_'
+     * Constructer for a new Training action.
+     * @param options stirng of options seperated by '_'
      *          "what=%s", unit type (worker or combatUnit)
-     *          "amount=%i", the amount to train
+     *          "amount=%i", the amount to train, integer > 0
      */
-    public TrainAction(String s){
-        actionName = BaseActionName.concat(s);
+    public TrainAction(String options){
+        if(options == null || options.isEmpty()){
+            actionName = BaseActionName.concat(options);
+
+        } else {
+            String[] inputOptionsList = options.split("_");
+
+            int i = 0;
+            if(inputOptionsList[i] == null || inputOptionsList[i].isEmpty()
+                    ||inputOptionsList[i].equals(BaseActionName)){
+                i++;
+            }
+
+            StringBuilder name = new StringBuilder(BaseActionName);
+            boolean validOption;
+
+            for(; i < inputOptionsList.length; i++){
+                validOption = false;
+                String[] currentOptionComponents = inputOptionsList[i].split("=");
+
+                //options is what to build
+                if(currentOptionComponents[0].equals("what") && currentOptionComponents.length == 2){
+                    for (int j = 0; j < possibleUnits.length; j++) {
+                        if(currentOptionComponents[1].equals(possibleUnits[j])){
+                            validOption = true;
+                        }
+                    }
+
+                //option is how much to build
+                } else if(currentOptionComponents[0].equals("amount") && currentOptionComponents.length == 2 ){
+                    Scanner numScann = new Scanner(currentOptionComponents[1]);
+                    if(numScann.hasNextInt()){
+                        validOption = numScann.nextInt() > 0;
+                    }
+                }
+
+                if(validOption){
+                    name.append('_');
+                    name.append(inputOptionsList[i]);
+                }
+                //else don't add it because the option is invalid.
+            }
+
+            this.actionName = name.toString();
+        }
     }
 
     /**
--- StarCraftBot/src/main/java/Planning/Actions/TrainActionType.java
+++ StarCraftBot/src/main/java/Planning/Actions/TrainActionType.java
@@ -23,9 +23,21 @@ public class TrainActionType implements ActionType {
     @Override
     public List<Action> allApplicableActions(State state) {
         List<Action> actions = new ArrayList<Action>(3);
-        actions.add(new TrainAction("_what=worker_amount=1"));
-        actions.add(new TrainAction("_what=combatUnit_amount=1"));
-        actions.add(new TrainAction());
+        int[][] currentCapacity = (int[][]) state.get("trainingCapacity");
+
+        //TODO: once actions are parsed in the bot, allow the bot to pass a command to train multiple units at once based on remaining capacity
+        //if we have the capacity to train workers.
+        if(currentCapacity[0][1] > 0){
+            actions.add(new TrainAction("_what=worker_amount=1"));
+        }
+
+        //if we have the capacity to train combat units
+        if(currentCapacity[1][1] > 0 || currentCapacity[2][1] > 0){
+            actions.add(new TrainAction("_what=combatUnit_amount=1"));
+        }
+
+        //actions.add(new TrainAction()); //commenting this out as it would create a lot of work to interperate in the bot.
+
         return actions;
     }
 }
