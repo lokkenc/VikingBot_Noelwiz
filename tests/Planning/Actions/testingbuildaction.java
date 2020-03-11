1b8fef92b99bad3be1e14c46c33c124b43755d04
peter346 <peter346@wwu.edu>
2020-03-11 13:44:20 -0700

    No longer needed

--- tests/Planning/Actions/testingbuildaction.java
+++
@@ -1,8 +0,0 @@
-package Planning.Actions;
-
-public class testingbuildaction extends BuildAction{
-
-        void SetActionName(String newName) {
-            this.actionName = newName;
-        }
-}
