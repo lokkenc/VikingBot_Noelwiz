844123ef6d7e0f0c8504f0e43352b51d159be7b0
peter346 <peter346@wwu.edu>
2020-03-11 13:41:52 -0700

    better wat if comparing

--- tests/Planning/Actions/BuildActionTest.java
+++ tests/Planning/Actions/BuildActionTest.java
@@ -11,31 +11,28 @@ class BuildActionTest {
         testingbuildaction control = new testingbuildaction();
         assertEquals(control.actionName(), new BuildAction().actionName(), "Defualt build action should be constructable.");
 
-        control = new testingbuildaction();
-        control.SetActionName("BuildAction_pop");
-        assertEquals(control.actionName(), new BuildAction("pop").actionName(), "pop is a valid option.");
+        assertEquals("BuildAction_pop", new BuildAction("pop").actionName(),
+                "pop is a valid option.");
+
+        assertEquals("BuildAction_pop", new BuildAction("_pop").actionName(),
+                "_pop is a valid option.");
+
+        assertEquals("BuildAction_pop", new BuildAction("BuildAction_pop").actionName(),
+                "a previous build action's name is a valid option.");
+
+        assertEquals("BuildAction_research", new BuildAction("research").actionName(),
+                "research is a valid option.");
+
+        assertEquals("BuildAction_train", new BuildAction("train").actionName(), "" +
+                "train is a valid option.");
 
         control = new testingbuildaction();
-        control.SetActionName("BuildAction_pop");
-        assertEquals(control.actionName(), new BuildAction("_pop").actionName(), "_pop is a valid option.");
+        assertEquals(control.actionName(), new BuildAction("nonsense this is invalid").actionName(),
+                "A nonsense option defaults to the base action name.");
 
         control = new testingbuildaction();
-        control.SetActionName("BuildAction_pop");
-        assertEquals(control.actionName(), new BuildAction("BuildAction_pop").actionName(), "a previous build action's name is a valid option.");
-
-        control = new testingbuildaction();
-        control.SetActionName("BuildAction_research");
-        assertEquals(control.actionName(), new BuildAction("research").actionName(), "research is a valid option.");
-
-        control = new testingbuildaction();
-        control.SetActionName("BuildAction_train");
-        assertEquals(control.actionName(), new BuildAction("train").actionName(), "train is a valid option.");
-
-        control = new testingbuildaction();
-        assertEquals(control.actionName(), new BuildAction("nonsense this is invalid").actionName(), "A nonsense option defaults to the base action name.");
-
-        control = new testingbuildaction();
-        assertEquals(control.actionName(), new BuildAction("").actionName(), "the empty string defaults to the base action name.");
+        assertEquals(control.actionName(), new BuildAction("").actionName(),
+                "the empty string defaults to the base action name.");
     }
 
 }
