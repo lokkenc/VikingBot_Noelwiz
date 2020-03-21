42e5528aeef3d55d36be7601c4cf58998c866ddb
bargert <bargert@wwu.edu>
2020-03-21 03:34:50 +0000

    Update StarcraftPlanner.java

--- StarCraftBot/src/main/java/Planning/StarcraftPlanner.java
+++ StarCraftBot/src/main/java/Planning/StarcraftPlanner.java
@@ -14,64 +14,64 @@ import burlap.statehashing.HashableStateFactory;
 import burlap.statehashing.ReflectiveHashableStateFactory;
 
 public class StarcraftPlanner {
-//    private Episode ep = new Episode();
-//
-//    private SparseSampling sparsePlanner;
-//    private Environment game;
-//    private Policy sparcePolicy;
-//
-//
-//    /**
-//     * intialize everything to use the ai planning.
-//     * @param initalstate (for now) a state representing the start of the game
-//     *                    so that stuff can be initalized.
-//     */
-//    public void Initalize(State initalstate){
-//        SADomain domain = new SADomain();
-//        //add actions to the domain
-//        domain.addActionType(new AttackActionType());
-//        domain.addActionType(new BuildActionType());
-//        domain.addActionType(new ExpandActionType());
-//        domain.addActionType(new ScoutActionType());
-//        domain.addActionType(new TrainActionType());
-//        domain.addActionType(new UpgradeActionType());
-//
-//        domain.setModel(new StarcraftModel());
-//
-//        HashableStateFactory factory = new ReflectiveHashableStateFactory();
-//
-//        //TODO: fill in reward function here
-//        ValueFunction initalreward = null;
-//
-//        //TODO: make sure the enviorment is initalized with everything it needs or something
-//        game = new StarcraftEnviorment();
-//
-//        //NOTE TO FUTURE SELVES: consider adjusting the discount factor.
-//        float DiscountFactor = 0.5f;
-//        sparsePlanner = new SparseSampling(domain,DiscountFactor,factory,10,1);
-//        sparsePlanner.setValueForLeafNodes(initalreward);
-//
-//        //get inital policy for planning
-//        sparcePolicy = new GreedyQPolicy(sparsePlanner);
-//
-//        //NOTE TO FUTURE SELVES: consider adjusting this.
-//        sparsePlanner.setForgetPreviousPlanResults(true);
-//    }
-//
-//    /**
-//     * Exicutes an action in the enviorment with the spare planner.
-//     */
-//    public void SparsePlanStep(){
-//        Action todo = sparcePolicy.action( game.currentObservation());
-//        game.executeAction( todo );
-//    }
-//
-//    /**
-//     *
-//     * @param vf A value funtion for evaluating
-//     */
-//    public void setGoal(ValueFunction vf){
-//        sparsePlanner.setValueForLeafNodes(vf);
-//        sparcePolicy = new GreedyQPolicy(sparsePlanner);
-//    }
+    private Episode ep = new Episode();
+
+    private SparseSampling sparsePlanner;
+    private Environment game;
+    private Policy sparcePolicy;
+
+
+    /**
+    * intialize everything to use the ai planning.
+    * @param initalstate (for now) a state representing the start of the game
+    *                    so that stuff can be initalized.
+    */
+    public void Initalize(State initalstate){
+        SADomain domain = new SADomain();
+        //add actions to the domain
+        domain.addActionType(new AttackActionType());
+        domain.addActionType(new BuildActionType());
+        domain.addActionType(new ExpandActionType());
+        domain.addActionType(new ScoutActionType());
+        domain.addActionType(new TrainActionType());
+        domain.addActionType(new UpgradeActionType());
+
+        domain.setModel(new StarcraftModel());
+
+        HashableStateFactory factory = new ReflectiveHashableStateFactory();
+
+        //TODO: fill in reward function here
+        ValueFunction initalreward = null;
+
+        //TODO: make sure the enviorment is initalized with everything it needs or something
+        game = new StarcraftEnviorment();
+
+        //NOTE TO FUTURE SELVES: consider adjusting the discount factor.
+        float DiscountFactor = 0.5f;
+        sparsePlanner = new SparseSampling(domain,DiscountFactor,factory,10,1);
+        sparsePlanner.setValueForLeafNodes(initalreward);
+
+        //get inital policy for planning
+        sparcePolicy = new GreedyQPolicy(sparsePlanner);
+
+        //NOTE TO FUTURE SELVES: consider adjusting this.
+        sparsePlanner.setForgetPreviousPlanResults(true);
+    }
+
+    /**
+    * Exicutes an action in the enviorment with the spare planner.
+    */
+    public void SparsePlanStep(){
+        Action todo = sparcePolicy.action( game.currentObservation());
+        game.executeAction( todo );
+    }
+
+    /**
+    *
+    * @param vf A value funtion for evaluating
+    */
+    public void setGoal(ValueFunction vf){
+        sparsePlanner.setValueForLeafNodes(vf);
+        sparcePolicy = new GreedyQPolicy(sparsePlanner);
+    }
 }
