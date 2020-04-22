2a8ca0ba6efa545ec401e47fe951c8c056c02a00
Matthew Carter <carter36@wwu.edu>
2020-04-22 15:56:15 -0700

    Add filtering methods to DataManager for specific data

--- StarCraftBot/src/main/java/exception/IncorrectActionTypeException.java
+++ StarCraftBot/src/main/java/exception/IncorrectActionTypeException.java
@@ -1,5 +1,8 @@
 package exception;
 
+/**
+ * The exception that is thrown when there's an unknown ActionType.
+ */
 public class IncorrectActionTypeException extends RuntimeException {
     public IncorrectActionTypeException(String errorMessage, Throwable err) {
         super(errorMessage, err);
