aa9600eb8a5e86d18049f763cb57c8783e21264e
peter346 <peter346@wwu.edu>
2020-05-28 22:19:27 -0700

    expanded queue size to 5

--- VikingBot/src/main/java/planning/StarcraftPlanner.java
+++ VikingBot/src/main/java/planning/StarcraftPlanner.java
@@ -69,6 +69,8 @@ public class StarcraftPlanner {
         SparsePlanStep();
         SparsePlanStep();
         SparsePlanStep();
+        SparsePlanStep();
+        SparsePlanStep();
     }
 
     /**
